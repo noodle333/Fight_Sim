a437eb7be4646191d13478814bd115f129b714aa
Oliver <oliver.nordin@elev.ga.ntig.se>
2021-09-23 22:13:41 +0200

    complete item class + insults

--- Fight_Sim/Item.cs
+++ Fight_Sim/Item.cs
@@ -10,6 +10,11 @@ namespace Fight_Sim
         //HEALING HAT (1hp per round), "Invisibility glasses(look really cool, and also dodges attack randomly), TAUNT MOUTHPIECE (GRANTS A CHANCE OF OPPONENT LOSING MATCH DUE TO CRYING)
         public List<string> specialItems = new List<string>{"Healing Hat", "Invisibility Glasses", "Taunt Mouthpiece"};
 
+        public List<string> tauntInsults = new List<string>{"Ain't no way your haircut that receeded at 20 bruh", "Ayo where'd you get those velcro ass shoes? Toys R Us?",
+        "Pickle chin ass boy", "Pickle head looking mf", "Boy your girlfriend look like my mom", "Damn you bout as dumb as Karl Pilkington", "You losing cause you blind af boy you be like Stevie, what's his name",
+        "DAMN BOY NO WONDER YOU AINT GOT NO WOMEN WALKING AROUND WITH THOSE BLAZERS", "OMM YOU THE UGLIEST MF IVE EVER SEEN SINCE I SAW MARK ZUCKERBERG FOR THE FIRST TIME", "BOY YOU LOOK LIKE A MIX BETWEEN A GECCO AND A SEAL",
+        "DAMN YOU GOT THEM LIZARD EYES BOY"};
+
         public void EnemyItem()
         {
             Random gen = new Random();
@@ -22,7 +27,7 @@ namespace Fight_Sim
             Console.WriteLine("What item would you like? Choose wisely.");
             Console.WriteLine("1: Healing Hat - Heals 1 point per round");
             Console.WriteLine("2: Invisibility Glasses - They look really cool, and you sometimes dodge attacks");
-            Console.WriteLine("3: Taunt Mouthpiece - Insult your opponent every round, small chance they end up crying and miss out on 5 rounds");
+            Console.WriteLine("3: Taunt Mouthpiece - Insult your opponent every round, small chance they end up crying and abandon the match");
             int pi = 0;
 
             while (pi < 1 || pi > 3)
@@ -34,19 +39,37 @@ namespace Fight_Sim
             Console.WriteLine($"You chose the {itemName}");
         }
 
-        public void ItemEffect()
+        public void ItemEffect(Fighter f, Fighter o)
         {
+            Random gen = new Random();
             if (itemName == specialItems[0])
             {
-
+                f.hp += 1;
+                Console.WriteLine($"{f.name}'s Healing Hat gave them +1 hp!");
             }
             else if (itemName == specialItems[1])
-            {
-
+            {   
+                int rDodge = gen.Next(1, 4);
+                if (rDodge == 3)
+                {
+                    //Fake dodge because I am very tired
+                    f.hp += 5;
+                    Console.WriteLine($"{f.name} dodged the attack!");       
+                }
             }
             else if (itemName == specialItems[2])
             {
-                
+                int rLoss = gen.Next(1, 20);
+                int rInsult = gen.Next(tauntInsults.Count);
+                Console.WriteLine($"{f.name} says: {tauntInsults[rInsult]}");
+                if (rLoss == 17)
+                {
+                    Console.WriteLine($"It's super effective! {o.name} starts crying and runs away!");
+                    o.hp = 0;
+                }
+                else {
+                    Console.WriteLine($"{o.name} doesn't seem bothered by the insult");
+                }
             }
         }
     }
